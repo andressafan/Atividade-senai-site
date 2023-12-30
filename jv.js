a75452a60862b36a1f98f90e41577279db9ed52b
Andressa <andressafelix@outlook.com>
2023-12-30 16:32:47 -0300

    ok

--- jv.js
+++ jv.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+  $('.loop').owlCarousel({
+      center: true,
+      items: 2,
+      loop: true,
+      margin: 10,
+      responsive: {
+          600: {
+              items: 5
+          }
+      }
   });
+});
+
