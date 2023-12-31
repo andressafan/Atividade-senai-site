65a81935c3f22f7c3144c054678af964983f38ed
Andressa <andressafelix@outlook.com>
2023-12-31 18:22:04 -0300

    ok

--- jv.js
+++ jv.js
@@ -1,15 +1,33 @@
-$(document).ready(function() {
-    $('.loop').owlCarousel({
-      center: true,
-      items: 2,
-      loop: true,
-      margin: 0,
-      responsive: {
-        600: {
-          items: 5
-        }
-      }
-    });
-  
-  
+function login() {
+  var logado = 0;
+  var usuario = document.getElementsByName('usuario')[0].value.toLowerCase();
+  var senha = document.getElementById('senha').value.toLowerCase();
+
+  if (usuario === "admin" && senha === "123456") {
+      window.location.href = "index.html";
+      logado = 1;
+  } else {
+      alert("Acesso Negado. Dados incorretos");
+  }
+}
+
+function cadastro() {
+  alert("Cadastrado com sucesso!");
+  window.location.href = "index.html";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("botao-cadastrar").addEventListener("click", function() {
+      document.getElementById("form-cadastrar").style.display = "block";
+      document.getElementById("section-login").style.display = "none";
+      document.getElementById("botao-logar").style.display = "block";
+      document.getElementById("botao-cadastrar").style.display = "none";
   });
+
+  document.getElementById("botao-logar").addEventListener("click", function() {
+      document.getElementById("form-cadastrar").style.display = "none";
+      document.getElementById("section-login").style.display = "block";
+      document.getElementById("botao-logar").style.display = "none";
+      document.getElementById("botao-cadastrar").style.display = "block";
+  });
+});
---
+++ owl-carousel-loop.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.loop').owlCarousel({
+      center: true,
+      items: 2,
+      loop: true,
+      margin: 0,
+      responsive: {
+        600: {
+          items: 5
+        }
+      }
+    });
+  
+  
+  });
